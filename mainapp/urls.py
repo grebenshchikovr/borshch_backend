e50186a77f39430bb0de830f1f06bbac4cdac91d
grebenshchikovr <grebenshchikov.r@gmail.com>
2019-12-08 05:28:07 +0300

    fix urls

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,8 @@ import mainapp.views as mainapp
 app_name = 'mainapp'
 
 urlpatterns = [
+    path('', mainapp.recipes, name='index'),
+    path('cuisine/<int:pk>/', mainapp.recipes, name='cuisine'),
+    path('recipe/<int:pk>', mainapp.recipe, name='recipe'),
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -6,8 +6,33 @@ def main(request):
     title = 'главная'
 
     recipes = Recipe.objects.all()
-    compositions = Composition.objects.all()
-    ingredients = Ingredient.objects.all()
 
-    content = {'title': title, 'recipes': recipes, 'compositions': compositions, 'ingredients': ingredients}
+    content = {'title': title, 'recipes': recipes,}
     return render(request, 'mainapp/index.html', content)
+
+def recipes(request, pk=None):
+    title = 'продукты'
+
+    recipes = Recipe.objects.all()
+    if pk != 0:
+        cuisine = get_object_or_404(Cuisine, pk=pk)
+        recipes = recipes.filter(cuisine__pk=pk).order_by('name')
+
+    content = {'title': title,
+               'recipes': recipes,
+               'cuisine': Cuisine.objects.all(),
+
+    }
+    return render(request, 'mainapp/recipes.html', content)
+
+def recipe(request, pk):
+    title = 'просмотр рецепта'
+    compositions = Composition.objects.filter(recipe_id=pk)
+    content = {
+        'title': title,
+        'recipe': get_object_or_404(Recipe, pk=pk),
+        'compositions': compositions,
+    }
+
+    return render(request, 'mainapp/recipe.html', content)
+
