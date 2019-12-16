0a0f4f79b2e639f18a401e79b27b0c8642ba7a71
grebenshchikovr <grebenshchikov.r@gmail.com>
2019-12-17 00:25:42 +0300

    т.к. не получается добавить функционал пользователей не дропнув базу, добавил скрипт по ее заполнению.

---
+++ mainapp/management/commands/fill_db.py
@@ -0,0 +1,69 @@
+from django.core.management.base import BaseCommand
+from mainapp.models import Cuisine, Unit, Ingredient, Recipe, Composition
+from django.contrib.auth.models import User
+
+import json, os
+
+JSON_PATH = 'mainapp/json'
+
+
+def loadFromJSON(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r', encoding="utf8") as infile:
+        return json.load(infile)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+
+        cuisines = loadFromJSON('cuisines')
+
+        Cuisine.objects.all().delete()
+        for cuisine in cuisines:
+            new_cuisine = Cuisine(**cuisine)
+            new_cuisine.save()
+
+        units = loadFromJSON('units')
+
+        Unit.objects.all().delete()
+        for unit in units:
+            new_unit = Unit(**unit)
+            new_unit.save()
+
+        ingredients = loadFromJSON('ingredients')
+
+        Ingredient.objects.all().delete()
+        for ingredient in ingredients:
+            unit_name = ingredient["unit"]
+            # Получаем единицу измерения по имени
+            _unit = Unit.objects.get(name=unit_name)
+            # Заменяем название единицы объектом
+            ingredient['unit'] = _unit
+            new_ingredient = Ingredient(**ingredient)
+            new_ingredient.save()
+
+        recipes = loadFromJSON('recipes')
+
+        Recipe.objects.all().delete()
+        for recipe in recipes:
+            cuisine_name = recipe["cuisine"]
+            # Получаем кухню по имени
+            _cuisine = Cuisine.objects.get(name=cuisine_name)
+            # Заменяем название кухни объектом
+            recipe['cuisine'] = _cuisine
+            new_recipe = Recipe(**recipe)
+            new_recipe.save()
+
+        compositions = loadFromJSON('compositions')
+
+        Composition.objects.all().delete()
+        for composition in compositions:
+            recipe_name = composition["recipe"]
+            _recipe = Recipe.objects.get(name=recipe_name)
+            composition['recipe'] = _recipe
+            ingredient_name = composition["ingredient"]
+            _ingredient = Ingredient.objects.get(name=ingredient_name)
+            composition['ingredient'] = _ingredient
+
+            new_composition = Composition(**composition)
+            new_composition.save()
