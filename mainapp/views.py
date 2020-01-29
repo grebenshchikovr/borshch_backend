925255a4f55fa0e6b152553c71ed939a75bebc2b
grebenshchikovr <grebenshchikov.r@gmail.com>
2020-01-29 22:11:07 +0300

    add duration filter form + some clean up

--- mainapp/views.py
+++ mainapp/views.py
@@ -25,20 +25,41 @@ class RecipeList(ListView):
 
     def get_queryset(self):
         query = self.request.GET.get('search')
-        filter_field = self.request.GET.get('filter_field')
-        if filter_field:
-            return Recipe.objects.filter(level=filter_field)
+        level = self.request.GET.get('level')
+        duration = self.request.GET.get('duration')
+        if level:
+            list = Recipe.objects.filter(level=level)
         else:
-            return Recipe.objects.all()
+            list = Recipe.objects.all()
+
+        if not duration:
+            pass
+        elif int(duration) <= 15:
+            list = list.filter(duration__range=(0, 15))
+        elif int(duration) > 15 and int(duration) <= 30:
+            list = list.filter(duration__range=(16, 30))
+        elif int(duration) > 30 and int(duration) <= 60:
+            list = list.filter(duration__range=(31, 60))
+        elif int(duration) > 60:
+            list = list.filter(duration__gte=60)
+
+        return list
+
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         # Add in a QuerySet of all the cuisines
         context['cuisine_list'] = Cuisine.objects.all()
+        # Add in a QuerySet form to choose level of recipe
         context['level_form'] = LevelFilterForm(initial={
             'search': self.request.GET.get('search', ''),
-            'filter_field': self.request.GET.get('filter_field', ''),
+            'level': self.request.GET.get('level', ''),
+        })
+        # Add in a QuerySet form to choose duration of recipe
+        context['duration_form'] = DurationFilterForm(initial={
+            'search': self.request.GET.get('search', ''),
+            'duration': self.request.GET.get('duration', ''),
         })
         return context
 
@@ -73,21 +94,23 @@ class CuisineRecipeList(RecipeList):
         return context
 
 class LevelFilterForm(forms.Form):
-    FILTER_CHOICES = (
+    level = (
         (None, 'Сложность'),
         ('1', 'Ребенок'),
         ('2', 'Взрослый'),
         ('3', 'Шеф-повар'),
     )
-    #search = forms.CharField(default='3')
-    filter_field = ChoiceField(choices=FILTER_CHOICES, required=False)
+
+    level = ChoiceField(choices=level, required=False)
 
 class DurationFilterForm(forms.Form):
     FILTER_CHOICES = (
         (None, 'Длительность'),
-        ('15', '15 минут'),
-        ('30', '30 минут'),
-        ('1', '1 час'),
+        (15, '15 минут'),
+        (30, '30 минут'),
+        (60, '1 час'),
+        (61, '> часа'),
     )
-    #search = models.CharField(default='3')
-    filter_field = ChoiceField(choices=FILTER_CHOICES, required=False)
+
+    duration = ChoiceField(choices=FILTER_CHOICES, required=False)
+
